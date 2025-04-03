f15be46b248b17c96ada372087827ad656ac76b8
Kelechi Ugwu <kcblack22@gmail.com>
2025-04-03 14:59:03 +0100

    Added more fields to the model

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,8 @@
 export interface IUser extends Document {
-    name: string;
-    email: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-  }
+  name: string;
+  email: string;
+  role?: "admin" | "user";
+  isActive?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
